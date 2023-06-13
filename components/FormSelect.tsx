b2baa425001a77522f963da607a56d0f32ced843
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-06-13 22:38:43 +0200

    replace timezone input with timezone dropdown

--- components/FormSelect.tsx
+++ components/FormSelect.tsx
@@ -1,16 +1,23 @@
 'use client'
 
-import {ChangeEventHandler} from "react";
+import React, {ChangeEventHandler} from "react";
 
-export default function FormSelect({name, options, className, required=false, label='My Label', otherAttrs={}, onChange}: {
+export default function FormSelect({name, options, className, required=false, label='My Label', otherAttrs={}, onChange=null, value=null}: {
   name: string,
   options: object,
   className?: string,
   required?: boolean,
   label?: string,
   otherAttrs?: object,
-  onChange: ChangeEventHandler
+  onChange?: ChangeEventHandler|null,
+  value?: any
 }) {
+
+  if (!onChange) {
+    onChange = function () {
+      return true;
+    }
+  }
 
   return (
     <div className={className}>
@@ -22,6 +29,7 @@ export default function FormSelect({name, options, className, required=false, la
         required={required}
         className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
         onChange={onChange}
+        value={value}
         {...otherAttrs}
       >
         {Object.keys(options).map(key => (
--- components/Optional.tsx
+++ components/Optional.tsx
@@ -1,7 +1,18 @@
 import Conditional from "@/components/Conditional";
 import FormInput from "@/components/FormInput";
+import FormSelect from "@/components/FormSelect";
+// @ts-ignore
+import timezones from 'google-timezones-json';
 
 export default function Optional({isPostgres}:{isPostgres: boolean})  {
+
+  // let defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  let emptyOption = {'': 'None'};
+  let myTimezones = {
+    ...emptyOption,
+    ...timezones
+  };
+
   return (
     <details className="my-5">
       <summary className="bg-inherit text-md cursor-pointer">Optional</summary>
@@ -11,7 +22,11 @@ export default function Optional({isPostgres}:{isPostgres: boolean})  {
           <FormInput type="text" name="schema" className="col-span-6" label="Schema"/>
         </Conditional>
 
-        <FormInput type="text" name="timezone" className="col-span-6" label="Timezone"/>
+        <FormSelect name="timezone"
+                    label="Timezone"
+                    className="col-span-6"
+                    options={myTimezones}
+        />
 
       </div>
     </details>
