7577e66981ecf6681bb75b6e12ce804ef86dbe40
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2024-09-22 12:02:58 +0200

    pnpm update + adjust deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
-#/bin/bash
 cd ~/cakephp-dsn-generator
 git checkout .
 git pull
+
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# enable PNPM packages
+export PATH="$HOME/.local/share/pnpm:$PATH"
+
+nvm use 22
 pnpm i
 pnpm run build
 pm2 restart dsn-generator
